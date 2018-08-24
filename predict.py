0f891e3ecb9266e22e5f68d6aab9dc8c4266fedc
buptlj <buptliujie@126.com>
2018-08-24 15:44:43 +0800

    predict the label of image

---
+++ predict.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import cv2
+import mnist
+import numpy as np
+
+
+def pred(filename, train_dir):
+    img = cv2.imread(filename, flags=cv2.IMREAD_GRAYSCALE)
+    img = tf.cast(img, tf.float32)
+    img = tf.reshape(img, [-1, 28, 28, 1])
+
+    logits, predict = mnist.inference(img, training=False)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state(train_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print('no checkpoint file')
+            return
+        pre = sess.run(predict)
+        print('model:{}, file:{}, label: {}, acc: {}'.
+              format(ckpt.model_checkpoint_path, filename, np.argmax(pre[0]), np.max(pre[0])))
+
+
+if __name__ == '__main__':
+    pred('./img_test/2_2098.jpg', './train')
